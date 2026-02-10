15786493f314366dc4e019167cbb5d2146f20fae
Hiba-N <hibanasir123@hotmail.com>
2026-02-10 11:01:10 -0500

    created Magic.Utilities.R and added instructions

---
+++ Magic.Utilities.R
@@ -0,0 +1,13 @@
+# # 1) Create a file named Magic.Utilities.R containing the following functions.
+
+# # 1a) Write a function which will take as an argument, at a minimum, 
+# the starting card of the trick. The function will play the game, and then return either 
+# the index or a string describing the last card that was found.
+
+# # 1b) Write a function that will
+
+# # set up the magic trick,
+# # randomly pick 2 cards from within the first 10 cards, one for the volunteer and one for the magician,
+# # run through the trick twice, once for the volunteer, and once for the magician.
+# # The function should return a boolean value, indicating whether or not the magician correctly 'guessed' the
+#  volunteer's final card. The sample and rep commands may be useful.
