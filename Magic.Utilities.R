5fb878afbadef258ecd681c34260c8b34ac0919b
Hiba-N <hibanasir123@hotmail.com>
2026-02-10 11:17:07 -0500

    created deck

--- Magic.Utilities.R
+++ Magic.Utilities.R
@@ -1,8 +1,24 @@
-# # 1) Create a file named Magic.Utilities.R containing the following functions.
+# # # 1) Create a file named Magic.Utilities.R containing the following functions.
 
-# # 1a) Write a function which will take as an argument, at a minimum, 
-# the starting card of the trick. The function will play the game, and then return either 
-# the index or a string describing the last card that was found.
+# # # 1a) Write a function which will take as an argument, at a minimum, 
+# # the starting card of the trick. The function will play the game, and then return either 
+# # the index or a string describing the last card that was found.
+
+# globa
+
+# card.magic <- function(init_card_index, deck) {
+     
+#      current_card_value <- deck[]
+#      current_card_position <- deck[]
+
+     
+
+#     return final_card
+# }
+
+
+magic.deck <- c(rep(c(4, 4, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), each = 4))
+#print(magic.deck) #sanity check
 
 # # 1b) Write a function that will
 
@@ -11,3 +27,5 @@
 # # run through the trick twice, once for the volunteer, and once for the magician.
 # # The function should return a boolean value, indicating whether or not the magician correctly 'guessed' the
 #  volunteer's final card. The sample and rep commands may be useful.
+
+
