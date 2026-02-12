792f3673e2627707faab3d15ab0b155036421680
Hiba-N <hibanasir123@hotmail.com>
2026-02-11 20:15:47 -0500

    completed Magic.Utilities.R

--- Magic.Utilities.R
+++ Magic.Utilities.R
@@ -1,32 +1,42 @@
-# # # 1) Create a file named Magic.Utilities.R containing the following functions.
-
-# # # 1a) Write a function which will take as an argument, at a minimum, 
-# # the starting card of the trick. The function will play the game, and then return either 
-# # the index or a string describing the last card that was found.
-
 
 #function that does the magic: takes in starting card and returns the final card
 magic.perform <- function(index, deck) {
-     while(index < 52){
-        index <- index + deck[[index]]
+     while (index < 52){
+        index <- index + deck[[index]] #jumping down the card row
      }
-    return (deck[[index]])
+    return (names(deck)[index]) #returning end card name
 }
 
 
 #function to set up cards, select players initial cards and compare players' final cards
 magic.setup <- function(){
 
-    deck <- c(rep(c(4, 4, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), each = 4)) #all cards in the deck
-    #print(magic.deck) #sanity check
 
-    #SHUFFLE THE CARDS FOR GODS SAKE
+   deck.suit <- c("2"= 2, "3"=3, "4"=4,"5"= 5, "6"=6, "7"=7, "8"= 8, "9"= 9, "10"= 10, "A"=1, "K"=4, "Q"=4, "J" =4)
+
+   deck.ace <- deck.suit
+   names(deck.ace) <- paste0(names(deck.ace), "A")
+
+   deck.spade <- deck.suit
+   names(deck.spade) <- paste0(names(deck.spade), "S")
+
+   deck.club <- deck.suit
+   names(deck.club) <- paste0(names(deck.club), "C")
+
+   deck.heart <- deck.suit
+   names(deck.heart) <- paste0(names(deck.heart), "H")
+
+   deck <- c(deck.ace, deck.spade, deck.club, deck.heart)
+
+   print(deck) #sanity check
+
+    deck.shuffled <- sample(deck)
 
     volunteer_index <- sample(1:10, 1)
     magician_index <- sample(1:10, 1)
 
-    volunteer.card <- magic.perform(volunteer_index, deck) #FIX THE FACT THAT 4 of K != 4 of Q
-    magician.card <- magic.perform(volunteer_index, deck)
+    volunteer.card <- magic.perform(volunteer_index, deck.shuffled) #FIX THE FACT THAT 4 of K != 4 of Q
+    magician.card <- magic.perform(volunteer_index, deck.shuffled)
 
     if (magician.card == volunteer.card){
         return (TRUE)
